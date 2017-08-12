636a8433b1a81fe926c7cfc6c1795d556d79a590
alex-dodoka <neksus.92@mail.ru>
2017-08-13 02:17:52 +0300

    Ready-made solution

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+const
+    URL = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}',
+    tableId = 'table_for_data',
+    requestType = 'GET';
+
+/*
+ * Делаем ajax запрос
+ * */
+function getDataFromUrl() {
+    $.ajax({
+        url: URL, // здесь подставляешь урл на который хочешь сделать запрос
+        type: requestType // здесь подставляешь тип запроса GET, POST, DELETE и т. д
+    })
+        .done((dataFromServer) => {
+            //  прогоняем массив объектов в цикле и по очереди их отрисоваваем
+            for (let i = 0; i < dataFromServer.length; i++) {
+                let html = drawData(dataFromServer[i]);
+                addHtml(tableId, html);
+            }
+        });
+}
+
+function drawData(modelData) {
+    return '<tr class="row-table">' + // навешиваю класс на каждый ряд таблицы, что бы потом по этом классу можно было очисть всю таблицу
+        '<td>' + modelData.id + '</td>' +
+        '<td>' + modelData.firstName + '</td>' +
+        '</tr>'
+}
+
+function addHtml(id, data) {
+    $("#" + id).append(data);
+}
+
+function clearTable(clazz) {
+    $("." + clazz).html('');
+}
